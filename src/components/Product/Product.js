096e8134fe9c5d3a01b78664438cb3750b4f6025
RayhanalKavey <rayhanalkavey@gmail.com>
2022-09-26 02:16:57 +0600

    Add comtent. Fectch data from SHOP component. Data lifting from PRODUCT component to its parent shop component, because we want to share information in the order summary, and order summary presents in the shop component. We use event handler to lift data, because in react data flows in a unidirection way so we cannot send data to the parent component with props.

---
+++ src/components/Product/Product.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Product.css";
+const Product = ({ handleAddToCart, product }) => {
+  // const Product = (props) => {
+  // const { handleAddToCart, product } = props;
+
+  const { name, price, img, ratings, seller, id, stock, category } = product;
+
+  return (
+    <div className="product">
+      <img src={img} alt="t-shirt" />
+      <div className="product-info">
+        <p style={{ marginTop: "0" }}>{name}</p>
+        <p>Price: ${price}</p>
+        <p style={{ marginTop: "20px" }}>
+          <small> Manufacturer: {seller}</small>
+        </p>
+        <p>
+          <small>Rating: {ratings}</small>
+        </p>
+      </div>
+      <button onClick={() => handleAddToCart(product)} className="btn-cart">
+        Add to cart
+      </button>
+    </div>
+  );
+};
+
+export default Product;
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import Product from "../Product/Product";
+import "./Shop.css";
+const Shop = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  const handleAddToCart = (product) => {
+    console.log(product);
+  };
+  return (
+    <div className="shop-container">
+      <div className="products-container">
+        {products.map((product) => (
+          <Product
+            product={product}
+            key={product.id}
+            handleAddToCart={handleAddToCart}
+          ></Product>
+        ))}
+      </div>
+      <div className="cart-container">
+        <h4>order summary</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;

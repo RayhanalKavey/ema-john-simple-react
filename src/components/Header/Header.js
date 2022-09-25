ee7c40ba1153d72629641b1a9ae43ed4ddb716eb
RayhanalKavey <rayhanalkavey@gmail.com>
2022-09-25 19:44:37 +0600

    Add header.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,17 @@
 import React from "react";
+import logo from "../../images/Logo.svg";
 import "./Header.css";
 const Header = () => {
   return (
-    <div>
-      <h2>This is header</h2>
-    </div>
+    <nav className="header">
+      <img src={logo} alt="" />
+      <div>
+        <a href="/shop">Shop</a>
+        <a href="/orders">Orders</a>
+        <a href="/inventory">Inventory</a>
+        <a href="/about">About</a>
+      </div>
+    </nav>
   );
 };
 
